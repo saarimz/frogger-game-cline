b53d960e200fa2bb62b4360e23925ae3f405b54c
Saarim Zaman <saarim.z@gmail.com>
2017-09-11 23:19:16 -0400

    added base classes for enemy and player

--- js/app.js
+++ js/app.js
@@ -1,43 +1,98 @@
 
-
-// Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+    constructor() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+        this.sprite = 'images/enemy-bug.png'
+        this.x = -100; 
+        this.y = Math.floor(Math.random() * 200) + 51;
+    }
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+         // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // all computers.this.x
+    
+        this.timeout = setTimeout(() => {
+                this.x++ * dt;
+        },1);
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-class Enemy {
-    constructor() {
+        if (this.x > 505) {
+            clearTimeout(this.timeout);
+        }
         
     }
+
+    render() {
+        // Draw the enemy on the screen, required method for game
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png'
+        this.x = 200;
+        this.y = 400;
+        this.gameOver = false;
+    }
+
+    update(dt) {
+
+        if (!this.gameOver) {
+            switch (this.key) {
+                case 'up':
+                    if (this.y >= 100) {
+                        this.y -= 100;
+                    }
+                    break;
+                case 'down':
+                    if (this.y < 400) {
+                        this.y += 100;
+                    }
+                    break;
+                case 'left':
+                    if (this.x >= 100) {
+                        this.x -= 100;
+                    }
+                    break;
+                case 'right':
+                    if (this.x < 400) {
+                        this.x += 100;
+                    }
+            }
+            this.key = null;
+            if (this.y === 0) {
+                this.gameOver = true;
+            }
+        }
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(key) {
+
+        this.key = key;
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [];
+allEnemies.push(new Enemy());
 
-
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
